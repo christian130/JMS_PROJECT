c9d8575cdc48671dc2f878f7a960418f0b99282e
Christian Vivas Santiago <christian130@gmail.com>
2016-07-27 16:58:43 -0400

    second commit of the day

---
+++ src/main/java/manager/DirectorDeOrquesta.java
@@ -0,0 +1,26 @@
+package manager;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("prototype")
+public class DirectorDeOrquesta implements Runnable{
+
+	
+	
+	public void run() {
+		
+		/*System.out.println();
+		
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		aqui tengo que colocar todo el
+		System.out.println();*/
+
+	}
+
+}
---
+++ src/main/java/manager/PoolDeConfiguracion.java
@@ -0,0 +1,21 @@
+package manager;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@ComponentScan(basePackages = "com.mkyong.thread")
+public class PoolDeConfiguracion {
+
+	@Bean
+	public ThreadPoolTaskExecutor taskExecutor() {
+		ThreadPoolTaskExecutor pool = new ThreadPoolTaskExecutor();
+		pool.setCorePoolSize(5);
+		pool.setMaxPoolSize(10);
+		pool.setWaitForTasksToCompleteOnShutdown(true);
+		return pool;
+	}
+
+}
